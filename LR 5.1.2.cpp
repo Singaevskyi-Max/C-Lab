f5bc185965572eee0b28d48233b165b2fcf0c42f
Singaevskyi-Max <76409361+Singaevskyi-Max@users.noreply.github.com>
2021-06-06 17:02:17 +0300

    Add files via upload

---
+++ LR 5.1.2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Square
+{
+public:
+    Square(double side);
+
+
+    void set_side(double arg);
+
+    void print();
+
+private:
+    double side;
+    double area;
+};
+Square::Square(double side)
+{
+    this->side = side;
+    this->area = side * side;
+}
+void Square::print()
+{
+    cout << "Square: side=" << this->side << " area=" <<this->area << endl;
+}
+
+void Square::set_side(double arg) {
+    if(arg>=0)
+    {
+        side=arg;
+        area=side*side;
+    }
+
+}
+
+int main()
+{
+    Square s(4);
+    s.print();
+    s.set_side(2.0);
+    s.print();
+    s.set_side(-33.0);
+    s.print();
+    return 0;
+}
